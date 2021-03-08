9e6118d2b43dc00bbcd278cd03eb22a617babc61
Alex Stashkov <sashastashkov@gmail.com>
2021-03-08 19:11:51 +0300

    Update Menu.cs
    Updated Menu

--- Assets/Scripts/Main Menu/Menu.cs
+++ Assets/Scripts/Main Menu/Menu.cs
@@ -2,17 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
-{   
+{
+    private Text _Credits;
+    private bool credits_trigger = true;
+
+    private void Start()
+    {
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            switch (transform.GetChild(i).name)
+            {
+                case "Credits":
+                    _Credits = transform.GetChild(i).GetComponent<Text>();
+                    break;
+            }
+        }
+        if (_Credits == null)
+        {
+            Debug.LogWarning("Main Menu could not locate Credits text.");
+        }
+    }
+
     public void Start_Button()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Level_01");
     }
 
     public void Coop_Button()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene("Coop_Level_01");
+    }
+
+    public void Credits_Button()
+    {
+        if (credits_trigger)
+        {
+            credits_trigger = false;
+            StartCoroutine(Credits());
+        }
+    }
+
+    private IEnumerator Credits()
+    {
+        _Credits.gameObject.SetActive(true);
+        yield return new WaitForSeconds(15f);
+        _Credits.gameObject.SetActive(false);
+        credits_trigger = true;
     }
 
     public void Exit_Button()
